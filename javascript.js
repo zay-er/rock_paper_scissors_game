10109ce2c60d6a2237c59c876f8b4b1c5ce03f27
zay-er <yarikmarietta@gmail.com>
2022-10-17 16:56:16 -0400

    Create rock paper scissors game

--- javascript.js
+++ javascript.js
@@ -1,13 +1,56 @@
-let computerSelection;
 
-let playerSelction = prompt('What do you choose?')
-
-const selection = ["Rock", "Paper", "Scissors"];
-
+const selection = ["rock", "paper", "scissors"];
 
 function computerChoice () {
     const random = Math.floor(Math.random() * selection.length);
-    console.log(selection[random]);
+    return (selection[random]);
 }
 
-computerChoice();
+let computerSelection = computerChoice();
+
+let playerSelction = prompt('What do you choose?').toLowerCase();
+
+function playRound(player, computer) {
+
+
+    if(player == computer){
+        console.log(computer);
+        console.log ('It is a tie!');
+    }
+
+    else if(player == 'rock' && computer == 'paper'){
+        console.log(`Computer chose ${computer}`);
+        console.log('Computer wins');
+    }
+
+    else if(player == 'paper' && computer == 'scissors'){
+        console.log(`Computer chose ${computer}`);
+        console.log('Computer wins');
+        
+    }
+    
+    else if(player == 'scissors' && computer == 'rock'){
+        console.log(`Computer chose ${computer}`);
+        console.log('Computer wins');
+        
+    }
+    
+    else if(computer == 'scissors' && player == 'rock'){
+        console.log(`Computer chose ${computer}`);
+        console.log('Player wins');
+            
+    }
+    else if(computer == 'paper' && player == 'scissors'){
+        console.log(`Computer chose ${computer}`);
+        console.log('player wins');
+            
+    } 
+    else if(computer == 'paper' && player == 'scissors'){
+        console.log(`Computer chose ${computer}`);
+        console.log('player wins');
+            
+        }
+
+ }
+
+playRound(playerSelction, computerSelection);
