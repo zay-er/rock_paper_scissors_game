968acc12562142250053758c6f501bd7a197fe3b
zay-er <yarikmarietta@gmail.com>
2022-10-17 15:51:18 -0400

    Add user input

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let computerSelection;
 
-let playerSelction;
+let playerSelction = prompt('What do you choose?')
 
 const selection = ["Rock", "Paper", "Scissors"];
 
@@ -9,6 +9,5 @@ function computerChoice () {
     const random = Math.floor(Math.random() * selection.length);
     console.log(selection[random]);
 }
-
 
 computerChoice();
