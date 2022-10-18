6bfae9b3958f1364f002a06b4d7bf015a28b641e
zay-er <yarikmarietta@gmail.com>
2022-10-18 12:22:32 -0400

    Create score keeping

--- javascript.js
+++ javascript.js
@@ -4,16 +4,12 @@ let playerScore = 0;
 
 let computerScore = 0;
 
-
 function computerChoice () {
     const random = Math.floor(Math.random() * selection.length);
     return (selection[random]);
 }
 
 function playRound(player, computer) {
-
-    
-
     if(player == computer){
         console.log(computer);
         console.log ('It is a tie!');
@@ -22,48 +18,52 @@ function playRound(player, computer) {
     else if(player == 'rock' && computer == 'paper'){
         console.log(`Computer chose ${computer}`);
         console.log('Computer wins');
+        computerScore++;
     }
 
     else if(player == 'paper' && computer == 'scissors'){
         console.log(`Computer chose ${computer}`);
         console.log('Computer wins');
-        
+        computerScore++;
     }
     
     else if(player == 'scissors' && computer == 'rock'){
         console.log(`Computer chose ${computer}`);
         console.log('Computer wins');
-        
+        computerScore++;
     }
     
     else if(computer == 'scissors' && player == 'rock'){
         console.log(`Computer chose ${computer}`);
         console.log('Player wins');
-            
+        playerScore++;
     }
     else if(computer == 'paper' && player == 'scissors'){
         console.log(`Computer chose ${computer}`);
         console.log('player wins');
-            
+        playerScore++;
     } 
     else if(computer == 'paper' && player == 'scissors'){
         console.log(`Computer chose ${computer}`);
         console.log('player wins');
-            
+        playerScore++;
         }
 
  }
 
 
 function game () { 
+
+
     for (let i = 0; i < 5; i++) {
     let computerSelection = computerChoice();
     let playerSelction = prompt('What do you choose?').toLowerCase();
     playRound(playerSelction, computerSelection);
-    
     }
 
-
+    if(playerScore > computerScore){
+        console.log
+    }
 }
 
 game ();
